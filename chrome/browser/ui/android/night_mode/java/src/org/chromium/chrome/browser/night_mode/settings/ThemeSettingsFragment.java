aba35dc343114daaf506deb9390048dcbcf4d661
Arnaud GRANAL <arnaud@geometry.ee>
2021-12-11 06:36:09 +0000

    Only display light and dark in theme settings

--- chrome/browser/ui/android/night_mode/java/src/org/chromium/chrome/browser/night_mode/settings/ThemeSettingsFragment.java
+++ chrome/browser/ui/android/night_mode/java/src/org/chromium/chrome/browser/night_mode/settings/ThemeSettingsFragment.java
@@ -77,6 +77,7 @@ public class ThemeSettingsFragment extends ChromeBaseSettingsFragment
                     getArguments().getInt(KEY_THEME_SETTINGS_ENTRY));
         }
 
+        if (false)
         if (ChromeFeatureList.isEnabled(
                 ChromeFeatureList.DARKEN_WEBSITES_CHECKBOX_IN_THEMES_SETTING)) {
             WebContentsDarkModeMessageController.notifyEventSettingsOpened(getProfile());
